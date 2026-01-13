c9a4f070878e805f04e6400aa848574351b68495
Eminecan Ozpamuk <cemine259@gmail.com>
2026-01-13 11:56:10 +0300

    feat: update hero section content and layout

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -3,12 +3,11 @@ import styles from "./Hero.module.scss";
 function Hero() {
   return (
     <header className={styles.hero}>
-      <p className={styles.text}>
-        <h3>Ahşap Üçgen Kedi Evi</h3>
-      </p>
+      <h1 className={styles.title}>Ahşap Üçgen Kedi Evi</h1>
 
       <p className={styles.text}>
-        Bahçenizde, sokağınızda, barınaklarda artık her yerde patili dostlarımıza destek olabilirsiniz.
+        Bahçenizde, sokağınızda, barınaklarda artık her yerde patili dostlarımıza
+        destek olabilirsiniz.
       </p>
 
       <hr className={styles.divider} />
