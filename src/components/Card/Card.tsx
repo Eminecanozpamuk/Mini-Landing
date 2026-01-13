47f0fd0ca2232450fd050bc72eb41e3cef785bee
Eminecan Ozpamuk <cemine259@gmail.com>
2026-01-13 11:55:58 +0300

    feat: improve card component structure

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -8,7 +8,14 @@ function Card({ image }: CardProps) {
   return (
     <div className={styles.card}>
       <div className={styles.imageArea}>
-        <img src={image} alt="Üçgen Kedi Evi" />
+        <img
+          src={image}
+          alt="Üçgen Kedi Evi"
+          loading="lazy"
+          decoding="async"
+          width="300"
+          height="200"
+        />
       </div>
       <div className={styles.infoArea}>
         <p>
